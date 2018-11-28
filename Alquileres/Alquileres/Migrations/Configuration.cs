139ff7b1174ef65899abbab4023d6b5fa24b8dab
heisemverg <mshermesjr@gmail.com>
2018-11-28 16:20:17 -0500

    Migracion Inicio1

---
+++ Alquileres/Alquileres/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Alquileres.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Alquileres.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Alquileres.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
