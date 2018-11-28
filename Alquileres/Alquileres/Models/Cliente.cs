4a1c62a4b39a04f317ed08e61591f54ebb79dee0
jefferson199721 <jeffersonmvinueza@gmail.com>
2018-11-28 15:34:18 -0500

    ClienteClase

--- Alquileres/Alquileres/Models/Cliente.cs
+++ Alquileres/Alquileres/Models/Cliente.cs
@@ -7,6 +7,16 @@ namespace Alquileres.Models
 {
     public class Cliente
     {
+        public int ClienteId { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Categoria { get; set; }
+        public string Garantia { get; set; }
+        public DateTime FechaIngreso { get; set; }
 
     }
 }
