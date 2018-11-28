a749809e69eafab1b6f8c2e3aab42c2077af6b3e
jefferson199721 <jeffersonmvinueza@gmail.com>
2018-11-28 17:04:40 -0500

    Vista

--- Alquileres/Alquileres/Controllers/ClientesController.cs
+++ Alquileres/Alquileres/Controllers/ClientesController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Alquileres.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +11,90 @@ namespace Alquileres.Controllers
 {
     public class ClientesController : Controller
     {
-        // GET: Clientes
+        ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Detalle(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Nuevo(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clientes.Add(cliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cliente);
+        }
+        public ActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cliente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cliente);
+        }
+        public ActionResult Eliminar(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+        [HttpPost]
+        public ActionResult Eliminar(int id)
+        {
+            Cliente cliente = db.Clientes.Find(id);
+            db.Clientes.Remove(cliente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
