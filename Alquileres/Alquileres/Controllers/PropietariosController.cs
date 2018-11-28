3dab4f065792867d7b772fd057f70ac50dc4fcca
Israel <israelgc.7.ic@icloud.com>
2018-11-28 18:38:27 -0500

    PropietarioModeloss

---
+++ Alquileres/Alquileres/Controllers/PropietariosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alquileres.Controllers
+{
+    public class PropietariosController : Controller
+    {
+        // GET: Propietarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
