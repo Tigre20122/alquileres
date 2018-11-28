3215014e481995e87dd168840b2de3934dd8e6eb
xavvier <xavicoeduar.p@hotmail.com>
2018-11-28 23:29:39 +0100

    INMUEBLES_ADQUILER

--- Alquileres/Alquileres/Models/Inmuebles.cs
+++ Alquileres/Alquileres/Models/Inmuebles.cs
@@ -22,7 +22,9 @@ namespace Alquileres.Models
         [Display(Name = "departamento")]
         [Required]
         public string departamento { get; set; }
-     
+        [Display(Name = "Tipo")]
+        [Required]
+        public string tipo { get; set; }
 
     }
 }
