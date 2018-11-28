72eeb80536e76c52c259346425e866631a2b479d
Israel <israelgc.7.ic@icloud.com>
2018-11-28 15:38:55 -0500

    ModeloPropietario1

--- Alquileres/Alquileres/Models/Propietario.cs
+++ Alquileres/Alquileres/Models/Propietario.cs
@@ -7,5 +7,15 @@ namespace Alquileres.Models
 {
     public class Propietario
     {
+        public int PropietarioId { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+
+       
     }
 }
