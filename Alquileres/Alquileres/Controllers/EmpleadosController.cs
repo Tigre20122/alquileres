96a651f2c18ac738130d6055b964ff8390d6f0ce
heisemverg <mshermesjr@gmail.com>
2018-11-28 18:11:37 -0500

    librerias

---
+++ Alquileres/Alquileres/Controllers/EmpleadosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Alquileres.Models;
+
+namespace Alquileres.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
