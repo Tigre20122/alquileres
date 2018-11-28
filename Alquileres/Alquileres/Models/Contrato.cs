e8382a8f95f74736983ac41462d6e6b320320d79
Tigre20122 <33558674+Tigre20122@users.noreply.github.com>
2018-11-28 18:21:35 -0500

    update _happy

---
+++ Alquileres/Alquileres/Models/Contrato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Alquileres.Models
+{
+    public class Contrato
+    {
+        [Key]
+        public int ContratoId { get; set; }
+    }
+}
