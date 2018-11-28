3a0914165dda7b249aa9a7b91cd00f5756f12b89
juanitoj <juanitojfm56@gmail.com>
2018-11-28 15:31:46 -0500

    Provincia

---
+++ Alquileres/Alquileres/Models/Provincia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alquileres.Models
+{
+    public class Provincia
+    {
+
+        public int ProvinciaId { get; set; }
+        public string Detalle { get; set; }
+
+    }
+}
