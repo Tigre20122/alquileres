7c7418c7b53dad63fe3846bb2cd62450fe9141d4
Tigre20122 <33558674+Tigre20122@users.noreply.github.com>
2018-11-29 08:46:37 -0500

    modelo contrato

--- Alquileres/Alquileres/Models/Contrato.cs
+++ Alquileres/Alquileres/Models/Contrato.cs
@@ -10,19 +10,25 @@ namespace Alquileres.Models
     {
         [Key]
         public int ContratoId { get; set; }
-
+        [Display(Name = "Cliente")]
+        [Required]
         public int ClienteId { get; set; }
         public Cliente Cliente { get; set; }
-
-        public int PropietarioId { get; set; }
-        public Propietario Propietario { get; set; }
-
+        [Display(Name = "Inmueble")]
+        [Required]
         public int InmuebleId { get; set; }
         public Inmuebles Inmuebles { get; set; }
-
+        [Display(Name = "FormaPago")]
+        [Required]
         public string ModoPago { get; set; }
+        [Display(Name = "Duracion del Contrato")]
+        [Required]
         public string DuracionContrato { get; set; }
+        [Display(Name = "Fecha Inicio")]
+        [Required]
         public DateTime FechaInicio { get; set; }
+        [Display(Name = "Fecha Fin")]
+        [Required]
         public DateTime FechaFin { get; set; }
     }
 }
