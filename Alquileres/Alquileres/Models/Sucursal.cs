7d2da450252ec56215405f07446dc22a124bf371
heisemverg <45246898+heisemverg@users.noreply.github.com>
2018-11-29 08:15:08 +0100

    actualizacion modelo surcusal

--- Alquileres/Alquileres/Models/Sucursal.cs
+++ Alquileres/Alquileres/Models/Sucursal.cs
@@ -11,17 +11,21 @@ namespace Alquileres.Models
         [Key]
         public int SucursalId { get; set; }
         [Display(Name="Empleado")]
+        [Required]
         public int EmpleadoId { get; set; }
         public Empleado Empleado { get; set; }
-
+        [Display(Name="Ciudad")]
+        [Required]
         public int CiudadId { get; set; }
         public ciudad ciudad { get; set; }
-
+        [Display(Name="Barrio")]
+        [Required]
         public string Barrio { get; set; }
-
+        [Display(Name="Barrio")]
+        [Required]
         public string calles { get; set; }
-
+        [Display(Name="Telefono")]
+        [Required]
         public string telefono { get; set; }
-
     }
 }
